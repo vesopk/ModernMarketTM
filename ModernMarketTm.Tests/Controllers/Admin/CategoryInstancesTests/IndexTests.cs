273f3b8e6b168e923bb512ecdc244366d56df400
Veselin <vesopk979@gmail.com>
2018-08-25 19:00:04 +0300

    adding more tests

---
+++ ModernMarketTm.Tests/Controllers/Admin/CategoryInstancesTests/IndexTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.Claims;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModernMarketTM.Data;
+using ModernMarketTM.Web.Areas.Admin.Controllers;
+using Moq;
+
+namespace ModernMarketTM.Tests.Controllers.Admin.CategoryInstancesTests
+{
+    [TestClass]
+    public class IndexTests
+    {
+        [TestMethod]
+        public void Index_IsInAdminRole()
+        {
+            var builder = new DbContextOptionsBuilder<ModernMarketTmDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var db = new ModernMarketTmDbContext(builder);
+
+            var mapper = new Mock<IMapper>().Object;
+
+            var controller = new CategoriesInstancesController(db, mapper)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = new DefaultHttpContext()
+                    {
+                        User = new ClaimsPrincipal(new ClaimsIdentity(new[]
+                        {
+                            new Claim(ClaimTypes.Role, "Admin")
+                        }))
+                    }
+                }
+            };
+
+            Assert.IsTrue(controller.User.IsInRole("Admin"));
+        }
+    }
+}
---
+++ ModernMarketTm.Tests/Controllers/Admin/DiscountsControllerTests/IndexTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Claims;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModernMarketTM.Data;
+using ModernMarketTM.Web.Areas.Admin.Controllers;
+using Moq;
+
+namespace ModernMarketTM.Tests.Controllers.Admin.DiscountsControllerTests
+{
+    [TestClass]
+    public class IndexTests
+    {
+        [TestMethod]
+        public void Index_IsInAdminRole()
+        {
+            var controller = new DiscountsController()
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = new DefaultHttpContext()
+                    {
+                        User = new ClaimsPrincipal(new ClaimsIdentity(new[]
+                        {
+                            new Claim(ClaimTypes.Role, "Admin")
+                        }))
+                    }
+                }
+            };
+
+            Assert.IsTrue(controller.User.IsInRole("Admin"));
+        }
+    }
+}
