975ec5550b11d1b884b384899538e0bd558cf707
Veselin <vesopk979@gmail.com>
2018-08-26 03:01:57 +0300

    adding more tests

---
+++ ModernMarketTm.Tests/Controllers/Admin/DiscountsControllerTests/ConfigureTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModernMarketTM.Web.Areas.Admin.Controllers;
+using ModernMarketTM.Web.Constants;
+
+namespace ModernMarketTM.Tests.Controllers.Admin.DiscountsControllerTests
+{
+    [TestClass]
+    public class ConfigureTests
+    {
+        [TestMethod]
+        public void Configure_DateTest()
+        {
+            var controller = new DiscountsController();
+
+            controller.Configure(DateTime.ParseExact("01-01-2018", "dd-MM-yyyy", CultureInfo.InvariantCulture));
+
+            var date = DiscountsContants.DiscountDate;
+
+            if (date != "1 януари.")
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void Configure_DateTestParsed()
+        {
+            var controller = new DiscountsController();
+
+            controller.Configure(DateTime.ParseExact("01-01-2018", "dd-MM-yyyy", CultureInfo.InvariantCulture));
+
+            var date = DiscountsContants.DiscountDateToBeParsed;
+
+            if (date != "1-1-2018")
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
---
+++ ModernMarketTm.Tests/Controllers/Admin/UsersControllerTests/ChangePasswordTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModernMarketTM.Data;
+using ModernMarketTM.Models;
+using ModernMarketTM.Web.Areas.Admin.Controllers;
+using ModernMarketTM.Web.Mapping;
+using Moq;
+
+namespace ModernMarketTM.Tests.Controllers.Admin.UsersControllerTests
+{
+    [TestClass]
+    public class ChangePasswordTests
+    {
+        private Mock<UserManager<User>> GetMockUserManager()
+        {
+            var userStoreMock = new Mock<IUserStore<User>>();
+            return new Mock<UserManager<User>>(
+                userStoreMock.Object, null, null, null, null, null, null, null, null);
+        }
+
+        [TestMethod]
+        public void ChangePassword()
+        {
+            var builder = new DbContextOptionsBuilder<ModernMarketTmDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var db = new ModernMarketTmDbContext(builder);
+            User user = new User()
+            {
+                UserName = "admin",
+                Email = "admin@example.com",
+                FullName = "Admin",
+                Address = "AdminHome N17"
+            };
+
+            Mapper.Initialize(config => config.AddProfile<MappingProfile>());
+
+            var manager = GetMockUserManager().Object;
+            db.Users.Add(user);
+            db.SaveChanges();
+
+            var controller = new UsersController(db,Mapper.Instance, manager);
+            var dbUser = db.Users.FirstOrDefault(u => u.UserName == "admin");
+
+            var action = controller.ChangePassword(dbUser.Id) as ViewResult;
+
+            Assert.IsNotNull(action.Model);
+        }
+    }
+}
